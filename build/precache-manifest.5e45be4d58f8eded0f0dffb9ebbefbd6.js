45b11602af31a413d1619175383ff6ceac7fa767
Rizwanul Hoque Ratul <rizwanulratul.192@gmail.com>
2020-03-19 11:51:29 +0600

    Version 0.1
    This is first version for GCP

---
+++ build/precache-manifest.5e45be4d58f8eded0f0dffb9ebbefbd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74f6f8d63f135e91afab8af19d5297a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d47e9621ba84c1577b96",
+    "url": "/static/js/2.4aa1f978.chunk.js"
+  },
+  {
+    "revision": "046558e6751caad2cc7ddb91f4013e3a",
+    "url": "/static/js/2.4aa1f978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3539de1ff74b2bc9b572",
+    "url": "/static/js/main.1bbd5e05.chunk.js"
+  },
+  {
+    "revision": "7813c506e284ec7fb09b",
+    "url": "/static/js/runtime-main.c2a4c0d3.js"
+  },
+  {
+    "revision": "06c50bce4d19c0b71268341f879d7936",
+    "url": "/static/media/google-logo.06c50bce.svg"
+  },
+  {
+    "revision": "62d2e6299a42e4cea2c8c75ac73239d2",
+    "url": "/static/media/twilio-logo.62d2e629.svg"
+  }
+]);
